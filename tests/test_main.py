75d95cbdeedc7aa0eea187cda9d91abdde50ed33
up26-dominic-lawrence-bermudez <114312907+up26-dominic-lawrence-bermudez@users.noreply.github.com>
2024-12-14 01:25:11 +0800

    test: add outline for I/O unit tests

---
+++ tests/test_main.py
@@ -0,0 +1,29 @@
+from io import StringIO
+from pytest import CaptureFixture, MonkeyPatch
+from src import main
+
+
+def test_sample_run(monkeypatch: MonkeyPatch, capfd: CaptureFixture[str]):
+    # pass string value to stdin for main.main()
+    test_input: str = '\n'.join(
+        ['3', '8', '8', '0', 'B;0;5;2;5;2;5', 'A;2;2;2;6', 'C;0;30']
+    )
+    monkeypatch.setattr('sys.stdin', StringIO(test_input))
+
+    # call main() from /src/main.py
+    main.main()
+
+    # capture stdout and stderr outputs from main.main()
+    out, err = capfd.readouterr()
+    assert out == '\n'.join(
+        [
+            # todo 1: figure out the actual output for this test case
+            # todo 2: maybe create a generic (stdin -> stdout, stderr) callback function
+            # todo 3: maybe implement [todo 2] by reading stdin from a .txt file, and comparing outputs with stdout/stderr from other .txt files
+            'Processes: [B, A, C]',
+            'PriorityQueues: [[], [], []]',
+            'Context Switch Time: 0',
+            '',
+        ]
+    )
+    assert err == ''
