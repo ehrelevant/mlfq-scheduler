8a753cd3785c8f442f966436c0085ea9c93cb3bb
up26-dominic-lawrence-bermudez <114312907+up26-dominic-lawrence-bermudez@users.noreply.github.com>
2024-12-18 16:45:58 +0800

    test: added MLFQ instantiation

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,22 @@
-from io import StringIO
-from pytest import CaptureFixture, MonkeyPatch
-from src import main
+#from io import StringIO
+from pytest import CaptureFixture#, MonkeyPatch
 
+#from src import main
+from src.main import MultiLevelFeedbackQueue, RRPriorityQueue, FCFSPriorityQueue, SJFPriorityQueue
+from src.main import Process
 
+def generate_mlfq(time_allotment_q1: int, time_allotment_q2: int, context_switch_time: int, processes: list[Process]) -> MultiLevelFeedbackQueue:
+    return MultiLevelFeedbackQueue(
+        processes,
+        [
+            RRPriorityQueue(time_allotment_q1),
+            FCFSPriorityQueue(time_allotment_q2),
+            SJFPriorityQueue(None),
+        ],
+        context_switch_time
+    )
+
+'''
 def test_sample_run(monkeypatch: MonkeyPatch, capfd: CaptureFixture[str]):
     # pass string value to stdin for main.main()
     test_input: str = '\n'.join(
@@ -14,6 +28,7 @@ def test_sample_run(monkeypatch: MonkeyPatch, capfd: CaptureFixture[str]):
     main.main()
 
     # capture stdout and stderr outputs from main.main()
+    out: str; err: str
     out, err = capfd.readouterr()
     assert out == '\n'.join(
         [
@@ -27,3 +42,31 @@ def test_sample_run(monkeypatch: MonkeyPatch, capfd: CaptureFixture[str]):
         ]
     )
     assert err == ''
+'''
+
+def test_mlfq_instantiation(capfd: CaptureFixture[str]) -> None:
+    time_allotment_q1: int = 8
+    time_allotment_q2: int = 8
+    context_switch_time: int = 0
+    processes: list[Process] = [
+        Process('B', 0, [5, 2, 5, 2, 5]),
+        Process('A', 2, [2, 2, 6]),
+        Process('C', 0, [30]),
+    ]
+
+    mlfq: MultiLevelFeedbackQueue = generate_mlfq(time_allotment_q1, time_allotment_q2, context_switch_time, processes)
+    print(mlfq)
+
+    # capture stdout and stderr outputs
+    out: str; err: str
+    out, err = capfd.readouterr()
+    assert out == '\n'.join(
+        [
+            'Processes: [B, A, C]',
+            'PriorityQueues: [[], [], []]',
+            'Context Switch Time: 0',
+            '',
+        ]
+    )
+    assert err == ''
+
